eb7c4d59f1f0bceb235e120da6a649d750eb1027
Disha Shetty <151986103+DishaShetty4@users.noreply.github.com>
2025-05-06 09:32:30 +0530

    Update Program_01.java

--- src/com/Test/Program_01.java
+++ src/com/Test/Program_01.java
@@ -4,6 +4,12 @@ public class Program_01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+             System.out.println("Hello World");
+		Scanner sc=next Scanner(System.in);
+		System.out.println("enter a number");
+		int n=s.nextInt();
+	      System.out.println("square" + (n*n));
+
 
 	}
 
